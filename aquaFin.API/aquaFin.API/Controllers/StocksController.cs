f1d42a4f377a8ec1950e85a7676f55650db91e66
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-10-07 06:16:54 +0530

    Created create stock controller

--- aquaFin.API/aquaFin.API/Controllers/StocksController.cs
+++ aquaFin.API/aquaFin.API/Controllers/StocksController.cs
@@ -1,4 +1,6 @@
 using aquaFin.API.AppDbContext;
+using aquaFin.API.Dtos.Stock;
+using aquaFin.API.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace aquaFin.API.Controllers;
@@ -15,7 +17,7 @@ public class StocksController:ApiControllerBase
     [HttpGet]
     public IActionResult GetAll()
     {
-     var stocks = _context.Stocks.ToList();
+     var stocks = _context.Stocks.ToList().Select(s=>s.ToStockDto());
      return Ok(stocks);
     }
 
@@ -28,5 +30,16 @@ public class StocksController:ApiControllerBase
             return NotFound();
         }
         return Ok(stock);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+    {
+        
+        var newStock = stockDto.ToStockFromCreateDto();
+        _context.Stocks.Add(newStock);
+        _context.SaveChanges();
+        return Ok(newStock);
+        
     }
 }
---
+++ aquaFin.API/aquaFin.API/Mappers/StockMappers.cs
@@ -0,0 +1,40 @@
+using aquaFin.API.Dtos.Stock;
+using aquaFin.API.Entities;
+
+namespace aquaFin.API.Mappers;
+
+public static class  StockMappers
+{
+    public static StockDto ToStockDto(this Stock stockEntity)
+    {
+        return new StockDto()
+        {
+            Id = stockEntity.Id,
+            Symbol = stockEntity.Symbol,
+            Purchase = stockEntity.Purchase,
+            CompanyName = stockEntity.CompanyName,
+            MarketCap = stockEntity.MarketCap,
+            LastDiv = stockEntity.LastDiv,
+            Industry = stockEntity.Industry,
+        };
+    }
+
+    public static Stock ToStockFromCreateDto(this CreateStockRequestDto stockRequestDto)
+    {
+        return new Stock()
+        {
+            MarketCap = stockRequestDto.MarketCap,
+            LastDiv = stockRequestDto.LastDiv,
+            Industry = stockRequestDto.Industry,
+            Purchase = stockRequestDto.Purchase,
+            CompanyName = stockRequestDto.CompanyName,
+            Symbol = stockRequestDto.Symbol, 
+            
+        };
+    }
+
+    public static string AnePanchoo(this string panchoo)
+    {
+        return panchoo+"Ane panchoo";
+    }
+}
